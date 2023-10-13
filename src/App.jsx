a6df655d5b4d62808411819457186dd6d9b80141
s-hens <108551327+s-hens@users.noreply.github.com>
2023-10-13 11:50:16 +0100

    Edit default React app page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -16,17 +15,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <h1>Hello, world!</h1>
+      <div>
+        <p>This page was made with Vite + React.</p>
+        <p>Edit <code>src/App.jsx</code> and save to test Hot Module Replacement (HMR)!</p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more.<br />
+        Or don't, I'm not the boss of you.
       </p>
     </>
   )
