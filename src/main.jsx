dd95e3264147df427703d428663d11a487458704
s-hens <108551327+s-hens@users.noreply.github.com>
2023-10-13 15:32:20 +0100

    Add another component

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Greeting from './greeting.jsx'
+import Pho from './pho.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Greeting />
+    <Pho />
   </React.StrictMode>,
 )
---
+++ src/pho.jsx
@@ -0,0 +1,13 @@
+import phopic from './assets/pho.gif'
+
+function Pho() {
+    return (
+    <>
+    <img src={phopic} alt='Delicious pho' height='200px' width='200px'/>
+    <br />
+    This page brought to you by my insatiable appetite for pho.
+    </>
+    )
+}
+
+export default Pho
